9bed9bf3db6bf994c6ee98ab733932beca73bcbc
Nishu Goel <n.goel@epilot.cloud>
2025-06-03 11:45:10 +0200

    feat: Publish provider

--- examples/resources/epilot-dashboard_dashboard/resource.tf
+++ examples/resources/epilot-dashboard_dashboard/resource.tf
@@ -1,8 +1,8 @@
-# resource "epilot-dashboard_dashboard" "my_dashboard" {
-#   id    = "3fa85f64-5717-4562-b3fc-2c963f66afa6"
-#   tiles = "{ \"see\": \"documentation\" }"
-#   title = "Employee Dashboard"
-# }
+resource "epilot-dashboard_dashboard" "my_dashboard" {
+  id    = "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+  tiles = "{ \"see\": \"documentation\" }"
+  title = "Employee Dashboard"
+}
 
 terraform {
   required_providers {
